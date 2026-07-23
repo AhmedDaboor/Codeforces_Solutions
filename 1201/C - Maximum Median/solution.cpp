4767adeeccf6f98303d7a7a43441969a0689f8c0
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-07-23 02:38:21 -0700

    Add Maximum Median [C] from Codeforces

---
+++ 1201/C - Maximum Median/solution.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+void print_int128(__int128 x);
+int msb(int x);
+int gcd(int a, int b);
+ll lcm(int a, int b);
+//_________________________________________________________________________________________________________________________________________
+ 
+void solve() {
+	int n, k; cin >> n >> k;
+	vector<int> a(n);
+	for(int&i:a) cin >> i;
+	sort(all(a));
+	int med = n/2;
+	auto can = [&](int c){
+		ll res = 0;
+		for(int i = med; i < n; i++){
+			res += max(0,c - a[i]);
+		}
+		return res;
+	};
+	int l = a[med], r = a[med]+k;
+	int ans = 0;
+	while(l <= r){
+		int mid = l + (r - l) / 2;
+		ll b = can(mid);
+		if(b <= k) {
+			ans = mid;
+			l = mid+1;
+		}
+		else r = mid-1;
+	}
+	cout << ans << endl;
+}
+ 
+ 
+int main() {
+    AHMED;
+    int t = 1;
+	//cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    }
+    reverse(all(s));
+    cout << s << endl;
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
