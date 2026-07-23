ae0528236a20cc0bbc574fe898eca24afeab5fd8
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-07-23 02:47:20 -0700

    Add Football [A] from Codeforces

---
+++ 43/A - Football/solution.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+void print_int128(__int128 x);
+int msb(int x);
+int gcd(int a, int b);
+ll lcm(int a, int b);
+//_________________________________________________________________________________________________________________________________________
+ 
+void solve() {
+	int n; cin >> n;
+	str team1, team2 = "";
+	int t1 = 0, t2 = 0;
+	cin >> team1;
+	t1++; n--;
+	while(n--){
+		str s; cin >> s;
+		if(s == team1) t1++;
+		else team2 = s, t2++;
+	}
+	if(t1 > t2) cout << team1 << endl;
+	else cout << team2 << endl;
+}
+ 
+ 
+int main() {
+    AHMED;
+    int t = 1;
+	//cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    }
+    reverse(all(s));
+    cout << s << endl;
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
