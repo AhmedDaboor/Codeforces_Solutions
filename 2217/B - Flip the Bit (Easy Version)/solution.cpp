fdfceca1d9ec3e0b8c3323f51ad41506a26ba00a
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-07-23 02:35:19 -0700

    Add Flip the Bit (Easy Version) [B] from Codeforces

---
+++ 2217/B - Flip the Bit (Easy Version)/solution.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+void print_int128(__int128 x);
+int msb(int x);
+int gcd(int a, int b);
+ll lcm(int a, int b);
+//_________________________________________________________________________________________________________________________________________
+ 
+void solve() {
+    int n, k; cin >> n >> k;
+    vi a(n+1);
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        cin >> a[i]; sum+=a[i];
+    }
+    int p; cin >> p;p--;
+    int x = a[p];
+    int L = 0, R = 0;
+    for (int i = p - 1; i >= 0; i--) {
+        if (a[i] != x && (i == p - 1 || a[i + 1] == x)) L++;
+    }
+    for (int i = p + 1; i < n; i++) {
+        if (a[i] != x && (i == p + 1 || a[i - 1] == x)) R++;
+    }
+        cout << 2 * max(L, R) << endl;
+}
+ 
+int main() {
+    AHMED;
+    // freopen("outofplace.in", "r", stdin);
+    // freopen("outofplace.out", "w", stdout);
+    int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    }
+    reverse(all(s));
+    cout << s << endl;
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
