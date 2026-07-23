384e8912552535fac13bac59bdf6d231b87be31c
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-07-23 02:33:31 -0700

    Add Magic Powder - 1 [D1] from Codeforces

---
+++ 670/D1 - Magic Powder - 1/solution.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+void print_int128(__int128 x);
+int msb(int x);
+int gcd(int a, int b);
+ll lcm(int a, int b);
+//_________________________________________________________________________________________________________________________________________
+ 
+void solve() {
+	ll n, k; cin >> n >> k;
+	vector<ll> a(n),b(n);
+	for(ll&i:a) cin >> i;
+	for(ll&i:b) cin >> i;
+	auto can = [&](ll m){
+		ll sum = 0;
+		for(int i = 0; i < n; i++){
+			ll need = a[i]*m - b[i];
+			if(need > 0) sum += need;
+			if(sum > k) return false; 
+		}
+		return true;
+	};
+	
+	int l = 0, r = 2e9;
+	ll ans = 0;
+	while(l <= r){
+		int mid = l + (r-l)/2;
+		if(can(mid)){
+			ans = mid;
+			l = mid+1;
+		}
+		else r = mid-1;
+	}
+	cout << ans << endl;
+}
+ 
+ 
+int main() {
+    AHMED;
+    int t = 1;
+	//cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    } 
+    reverse(all(s));
+    cout << s << endl;
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
