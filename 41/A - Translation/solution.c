6e05800a2c3c311e453bb6cc23ed1bc1c6b72784
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-07-28 08:16:36 -0700

    Add Translation [A] from Codeforces

---
+++ 41/A - Translation/solution.c
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            int t = 1;
+            //t = int.Parse(Console.ReadLine());
+            while (t-- != 0) {
+                Solve();
+            }
+        }
+        public static void Solve() {
+            string s = Console.ReadLine();
+            string r = Console.ReadLine();
+            string revs = string.Concat(s.Reverse());
+            if (r == revs) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
