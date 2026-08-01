5c9105b78996652aaf7d8d45f3e22591dd42deef
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-08-01 00:14:13 -0700

    Add Don't Try to Count [A] from Codeforces

---
+++ 1881/A - Don't Try to Count/solution.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+void print_int128(__int128 x);
+int msb(int x);
+int gcd(int a, int b);
+ll lcm(int a, int b);
+//_________________________________________________________________________________________________________________________________________
+ 
+void solve() {
+	int n, m; cin >> n >> m;
+	str x, s; cin >> x >> s;
+	int ans = 0;
+	while(n < m){
+		x += x;
+		n = x.size();
+		ans++;
+	}
+	if(x == s) {cout << ans << endl; return;}
+	if(n==m) {
+		x+=x;
+		n = x.size();
+		ans++;
+	}
+	auto check = [&](){
+		for(int i = 0; i < n; i++){
+			for(int j = i; j < n; j++){
+				if(x.substr(i,j-i+1) == s) return true;
+			}
+		}
+		return false;
+	};
+	if(check()) {cout << ans << endl; return;}
+	else {
+		x+=x;
+		n = x.size();
+		ans++;
+	}
+	if(check()) {cout << ans << endl; return;}
+	cout << -1 << endl;
+}
+ 
+int main() {
+    AHMED;
+    // freopen("breedflip.in","r",stdin);
+    // freopen("breedflip.out","w",stdout);
+    int t = 1;
+	cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    } 
+    reverse(all(s));
+    cout << s << endl;
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
