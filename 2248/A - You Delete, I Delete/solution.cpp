b0fc0cad2420287005f2d438e1f6560e282f8824
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-08-01 07:47:24 -0700

    Add You Delete, I Delete [A] from Codeforces

---
+++ 2248/A - You Delete, I Delete/solution.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+void print_int128(__int128 x);
+int msb(int x);
+int gcd(int a, int b);
+ll lcm(int a, int b);
+//_________________________________________________________________________________________________________________________________________
+ 
+void solve() {
+	str s; cin >> s;
+	for(int i = 0; i < (int)s.size(); i++){
+		if(s[i]=='1') {s.erase(i,1); break;}
+	}
+	for(int i = 0; i < (int)s.size(); i++){
+		if(s[i]=='0') {s.erase(i,1); break;}
+	}
+	cout << s << endl;
+}
+ 
+int main() {
+    AHMED;
+    // freopen("breedflip.in","r",stdin);
+    // freopen("breedflip.out","w",stdout);
+    int t = 1;
+	cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    } 
+    reverse(all(s));
+    cout << s << endl;
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
