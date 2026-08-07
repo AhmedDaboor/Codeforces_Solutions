ab8e51aff0e6cffb65204650105773ab2953bf7d
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-08-07 07:47:43 -0700

    Add The Best Card [A] from Codeforces

---
+++ 2253/A - The Best Card/solution.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+void print_int128(__int128 x);
+int msb(int x);
+int gcd(int a, int b);
+ll lcm(int a, int b);
+template <typename T>
+inline void vecin(vector<T> &a);
+template <typename T>
+inline void vecout(const vector<T> &a);
+//_________________________________________________________________________________________________________________________________________
+ 
+void solve() {
+	int n; cin >> n;
+	auto isPrime = [&](int m){
+		if(m<2) return false;
+		for(int i = 2; i*i <= m; i++){
+			if(m%i==0) return false; 
+		}
+		return true;
+	};
+	cout << (isPrime(n+1) ? "Yes" : "No") << endl;
+}
+int main() {
+    AHMED;
+    // freopen("breedflip.in","r",stdin);
+    // freopen("breedflip.out","w",stdout);
+    int t = 1;
+	cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    } 
+    reverse(all(s));
+    cout << s << endl; 
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
+ 
+template <typename T>
+inline void vecin(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+ 
+template <typename T>
+inline void vecout(const vector<T> &a)
+{
+    for (const auto &x : a)
+        cout << x << ' ';
+    cout << '
+';
+}
