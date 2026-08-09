5486b36a6ad25f92734f9fc702de88b366e5661f
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-08-09 09:41:04 -0700

    Add Hot Potatoes at the Fairy Warehouse [C] from Codeforces

---
+++ 2256/C - Hot Potatoes at the Fairy Warehouse/solution.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+void print_int128(__int128 x);
+int msb(int x);
+int gcd(int a, int b);
+ll lcm(int a, int b);
+ll power(ll base, ll exp);
+template <typename T>
+inline void vecin(vector<T> &a);
+template <typename T>
+inline void vecout(const vector<T> &a);
+//_________________________________________________________________________________________________________________________________________ 
+const ll MOD = 998244353;
+void solve() {
+    ll n, k; cin >> n >> k;
+    int m = 2 * n;
+    str s; cin >> s;
+    str t = s;
+    for (int i = 0; i < m; i++) {
+    	int nxt = (i + 1) % m;
+        if (s[i] == '1' && s[nxt] == '0') {
+            t[i] = '0';
+            t[nxt] = '1';
+        }
+    }
+    s = t; 
+    int red = 0, blue = 0;
+    for (int i = 0; i < m; i++) {
+        if (s[i] == '1') {
+            if ((i + 1) % 2 == 0) red++; 
+            else blue++;                
+        }
+    }
+    cout << red << ' ' << blue << endl;
+}
+int main() {
+    AHMED;
+    // freopen("breedflip.in","r",stdin);
+    // freopen("breedflip.out","w",stdout);
+    int t = 1;
+	cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    } 
+    reverse(all(s));
+    cout << s << endl; 
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
+ 
+template <typename T>
+inline void vecin(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+ 
+template <typename T>
+inline void vecout(const vector<T> &a)
+{
+    for (const auto &x : a)
+        cout << x << ' ';
+    cout << '
+';
+}
+ 
+ll power(ll base, ll exp) {
+    ll res = 1;
+    base %= MOD;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            res = (res * base) % MOD;
+        }
+        base = (base * base) % MOD; 
+        exp /= 2;
+    }
+    return res;
+}
