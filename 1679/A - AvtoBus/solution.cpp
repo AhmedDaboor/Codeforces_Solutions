3d5c626ecbe7baae5fa4a2e530299bfe8c8ed465
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-08-19 12:20:18 +0300

    Add AvtoBus [A] from Codeforces

---
+++ 1679/A - AvtoBus/solution.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+const ll MOD = 1e18;
+void print_int128(__int128 x);
+int msb(int x); 
+int gcd(int a, int b);
+ll lcm(int a, int b);
+ll power(ll base, ll exp);
+template <typename T>
+inline void vecin(vector<T> &a);
+template <typename T>
+inline void print(const vector<T> &a);
+//_________________________________________________________________________________________________________________________________________ 
+void solve() {
+    ll n; cin >> n;
+	if(n < 4 || (n%4!=2 && n%4!=0) || (n%6!=2 && n%6!=0 && n%6!=4)) {cout << -1 << endl; return;}
+	cout << n/6 + (n%6 == 0 ? 0 : 1) << ' ' << n/4 << endl; 
+}
+ 
+int main() {
+    AHMED;
+    int t = 1; cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    } 
+    reverse(all(s));
+    cout << s << endl; 
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
+ 
+template <typename T>
+inline void vecin(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+ 
+template <typename T>
+inline void print(const vector<T> &a)
+{
+    for (const auto &x : a)
+        cout << x << ' ';
+    cout << '
+';
+}
+ 
+ll power(ll base, ll exp) {
+    ll res = 1;
+    base %= MOD;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            res = (res * base) % MOD;
+        }
+        base = (base * base) % MOD; 
+        exp /= 2;
+    }
+    return res;
+}
