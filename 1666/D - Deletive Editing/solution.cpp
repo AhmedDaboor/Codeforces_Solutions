311b272115bc8bf0a05218b50b9586976fc118c3
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-08-20 09:27:22 +0300

    Add Deletive Editing [D] from Codeforces

---
+++ 1666/D - Deletive Editing/solution.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define endl '
+'
+const ll INF = 1e18;
+const ll MOD = 1e18;
+void print_int128(__int128 x);
+int msb(int x); 
+int gcd(int a, int b);
+ll lcm(int a, int b);
+ll power(ll base, ll exp);
+template <typename T>
+inline void vecin(vector<T> &a);
+template <typename T>
+inline void print(const vector<T> &a);
+//_________________________________________________________________________________________________________________________________________ 
+void solve() {
+	str s, t; cin >> s >>  t;
+	vi fs(26), ft(26), d(26);
+	for(char&c:s) fs[c-'A']++;
+	for(char&c:t) ft[c-'A']++;
+	for(int i = 0; i < 26; i ++){
+		d[i] = fs[i] - ft[i];
+		if(d[i]<0){cout << "NO" << endl; return;}
+	}
+	for(int i = 0; i < 26; i++){
+		if(d[i]==0) continue;
+		int n = int(s.size());
+		vi pos;
+		for(int j = 0; j < n && d[i]; j++){
+			if(s[j] == int('A' + i)){
+				pos.pb(j);
+				d[i]--;
+			}
+		}
+		for(int j = pos.size()-1; j >= 0; j--){
+			s.erase(pos[j],1);
+		}
+	}
+	if(s==t) cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
+int main() {  
+    AHMED;
+    int t = 1; cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0"; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    } 
+    reverse(all(s));
+    cout << s << endl; 
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+int gcd(int a,  int b) {
+    while(b != 0) {
+        int temp =  b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+ 
+ll lcm(int a, int b) {
+    return (ll)(a/gcd(a, b))*b;
+}
+ 
+template <typename T>
+inline void vecin(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+ 
+template <typename T>
+inline void print(const vector<T> &a)
+{
+    for (const auto &x : a)
+        cout << x << ' ';
+    cout << '
+';
+}
+ 
+ll power(ll base, ll exp) {
+    ll res = 1;
+    base %= MOD;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            res = (res * base) % MOD;
+        }
+        base = (base * base) % MOD; 
+        exp /= 2;
+    }
+    return res;
+}
