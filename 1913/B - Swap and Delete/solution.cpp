160936a6c98d459e2d0ae5020ab6aaffac1aa15f
Ahmed Daboor <ahmeddabour818@gmail.com>
2026-08-25 12:35:29 +0300

    Add Swap and Delete [B] from Codeforces

---
+++ 1913/B - Swap and Delete/solution.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+using str = string;
+using i128 = __int128_t;
+#define AHMED ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define gcd(a,b) __gcd(a,b)
+#define endl '
+'
+const ll INF = 1e18;
+const ll MOD = 1e9+7;
+void print_int128(__int128 x);
+int msb(int x); 
+ll power(ll base, ll exp);
+template <typename T>
+inline void vecin(vector<T> &a);
+template <typename T>
+inline void print(const vector<T> &a);
+//_________________________________________________________________________________________________________________________________________ 
+void solve() {
+	str s; cin >> s;
+	ll n = s.size();
+	ll zeros = 0, ones = 0;
+	for(char&c:s) if(c=='0') zeros++; else ones++;
+	for(int i = 0; i < n; i++){
+		if(s[i] == '0'){
+			if(ones==0) {cout << n - i << endl; return;}
+			ones--;
+		}
+		else {
+			if(zeros==0) {cout << n-i << endl; return;}
+			zeros--;
+		}
+	}
+	cout << 0 << endl;
+} 
+int main() {  
+    AHMED;
+    int t = 1; cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+//___________________________________________________________________________________________________________________________________________
+void print_int128(__int128 x) {
+    if (x == 0) { cout << "0" << endl; return; }
+    if (x < 0) { cout << '-'; x = -x; }
+    string s;
+    while (x > 0) {
+        int digit = (int)(x % 10);
+        s.pb(char('0' + digit));
+        x /= 10;
+    } 
+    reverse(all(s));
+    cout << s << endl; 
+}
+ 
+int msb(int x){
+    if(x==0) return -1;
+    return 31 - __builtin_clz(x);
+}
+ 
+template <typename T>
+inline void vecin(vector<T> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+}
+ 
+template <typename T>
+inline void print(const vector<T> &a)
+{
+    for (const auto &x : a)
+        cout << x << ' ';
+    cout << '
+';
+}
+ 
+ll power(ll base, ll exp) {
+    ll res = 1;
+    base %= MOD;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            res = (res * base) % MOD;
+        }
+        base = (base * base) % MOD; 
+        exp /= 2;
+    }
+    return res;
+}
